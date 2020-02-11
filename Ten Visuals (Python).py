0a1c1ba58e92d2c46fbdd182573df1048e184a07
Tap Suksumrun <ratanon97@users.noreply.github.com>
2020-02-11 17:15:21 +0000

    Plot Histogram of thermostatic rebates

--- Ten Visuals (Python).py
+++ Ten Visuals (Python).py
@@ -15,3 +15,13 @@ print(os.getcwd())
 os.chdir("C:\\Users\\KOMSUN\\Documents\\GitHub\\TenVisualisations\\Data Sources")
 ThermoStat  = pd.read_csv("thermostat_rebates_by_zip_1000.csv")
 #-------------------------------------------------------------------------------
+#1. Histogram
+#Plotting the thermostatic rebate (USD)
+sns.set_style("darkgrid")
+fig, ax = plt.subplots() #Put the whole graph in a "subplot" but will still give a normal graph #HACK
+fig.set_size_inches(11.7,8.27)
+plt.hist(ThermoStat["rebate-usd"],bins=15)
+plt.title('Histogram of Thermostatic Rebates (USD)', fontsize = 20)
+plt.xlabel('Rebate (USD)',fontsize = 14)
+plt.ylabel('Frequency',fontsize = 14)
+plt.show()
